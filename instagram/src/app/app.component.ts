2391abe129d6ab8635c085a705215d819f004943
Enrique <eprado@miralarana.com>
2023-11-04 12:45:51 +0100

    cambios instagram angular

--- instagram/src/app/app.component.ts
+++ instagram/src/app/app.component.ts
@@ -6,5 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'instagram';
+  title: string = 'instagram';
+  publicaciones: number = 0;
+  seguidores: number = 300;
+  siguiendo: number = 200;
+
+
+  cargardatos() {
+    this.publicaciones = Math.random();
+    this.seguidores = Math.random();
+    this.siguiendo = Math.random();
+  }
+  contarpublicaciones() {
+    this.publicaciones = this.publicaciones + 1;
+    this.calcularseguidores();
+  }
+
+  calcularseguidores() {
+    this.seguidores = this.publicaciones * 2;
+    this.siguiendo = this.publicaciones * (1 + 1 / 3);
+    this.siguiendo = Math.round(this.siguiendo);
+  }
+
 }
