22524d6313c262571b87e124d2d9428160c31853
Marta Grana <marta.grana.fdez@gmail.com>
2023-11-12 13:44:48 +0100

    nuevos cambios

--- instagram/src/app/app.component.ts
+++ instagram/src/app/app.component.ts
@@ -8,24 +8,47 @@ import { Component } from '@angular/core';
 export class AppComponent {
 
   title: string = 'instagram';
-  usuario: string = 'Death_cab';
-  nombrereal: string = 'Marta';
-  fotousuario: string = 'instagram\src\assets\img\marta-grana.jpg';
+
   titulodato1: string = 'publicaciones';
   titulodato2: string = 'seguidores';
   titulodato3: string = 'siguiendo';
   publicaciones: number = 0;
   seguidores: number = 300;
   siguiendo: number = 200;
+  fechanacimiento: Date = new Date(1983, 11, 16);
+  eschica: boolean = true;
+
+  perfil = {
+    usuario: 'Death_cab',
+    nombrereal: 'Marta Graña',
+    fotousuario: 'assets/img/marta-grana.jpg',
+  };
+
 
   cargardatos() {
     this.publicaciones = Math.random();
     this.seguidores = Math.random();
     this.siguiendo = Math.random();
+
   }
   contarpublicaciones() {
     this.publicaciones = this.publicaciones + 1;
     this.calcularseguidores();
+    console.log(this.fechanacimiento.getFullYear());
+    if (this.eschica) {
+      console.log("soy chica");
+      let añonacimiento: number = this.fechanacimiento.getFullYear();
+      if (añonacimiento > 1980) { console.log("soy de los ochenta"); }
+      else {
+        console.log("no soy de los ochenta");
+      }
+    } else {
+      console.log("no soy chica");
+
+    };
+    console.log(this.perfil);
+
+
   }
 
   calcularseguidores() {
